cc1e135764dfda9f0daf8b080321658a4a17b413
Halcolt <billtrinh19@gmail.com>
2024-12-30 15:59:54 +0700

    refactor

--- src/main/java/controller/order/OrderScreenController.java
+++ src/main/java/controller/order/OrderScreenController.java
@@ -1,16 +1,19 @@
 package controller.order;
 
-import controller.OrderController;
 import controller.common.BaseScreenController;
 import controller.payment.ResultScreenController;
 import entity.order.Order;
+import entity.order.entities.RefundTransaction;
+import entity.payment.TransactionResult;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import subsystem.vnPay.VnPaySubsystemController;
 import utils.Configs;
+import utils.enums.OrderStatus;
 
 import java.io.File;
 import java.io.IOException;
@@ -37,9 +40,7 @@ public class OrderScreenController extends BaseScreenController {
             homeScreenHandler.show();
         });
     }
-    public OrderController getBController() {
-        return (OrderController) super.getBController();
-    }
+
     public void show(BaseScreenController prevScreen) throws SQLException {
         setPreviousScreen(prevScreen);
         setScreenTitle("Order Screen");
@@ -48,18 +49,15 @@ public class OrderScreenController extends BaseScreenController {
         show();
     }
 
-    private void displayOrder(){
+    private void displayOrder() {
         vboxOrder.getChildren().clear();
-        var listOrders = getBController().getOrders();
+        Order orderInstance = new Order(); // Create an instance
+        var listOrders = orderInstance.getListOrders(); // Use instance method
 
         try {
             for (var cm : listOrders) {
-
-
                 OrderMediaController orderMediaHandler = new OrderMediaController(Configs.ORDER_MEDIA_PATH, this);
                 orderMediaHandler.setOrder(cm);
-
-                // add spinner
                 vboxOrder.getChildren().add(orderMediaHandler.getContent());
             }
         } catch (IOException e) {
@@ -67,15 +65,49 @@ public class OrderScreenController extends BaseScreenController {
         }
     }
 
-    public void handleRejectOrder(Order order) throws IOException {
-        var response = getBController().cancelOrder(order);
-        BaseScreenController resultScreen = new ResultScreenController(this.stage, Configs.RESULT_SCREEN_PATH,
-                response.getResult(), response.getMessage());
 
+    public void handleRejectOrder(Order order) throws IOException {
+        TransactionResult result = new TransactionResult();
+
+        if (!order.getStatus().equals(OrderStatus.Rejected)) {
+            try {
+                var trans = order.getPaymentTransaction();
+                var refundParams = new RefundTransaction(
+                        "02",
+                        String.valueOf(trans.getAmount()),
+                        trans.getTxnRef(),
+                        trans.getTransactionNo(),
+                        trans.getCreatedAt().toString(),
+                        order.getName()
+                );
+
+                VnPaySubsystemController vnPayService = new VnPaySubsystemController();
+                vnPayService.refund(refundParams);
+
+                result.setResult("SUCCESS");
+                result.setMessage("REFUND SUCCESS, PLEASE CHECK YOUR BANK");
+                order.updateStatus(OrderStatus.Rejected, order.getId());
+            } catch (Exception e) {
+                result.setResult("REFUND FAILED");
+                result.setMessage(e.getMessage());
+            }
+        } else {
+            result.setResult("FAILED");
+            result.setMessage("Có lỗi xảy ra, vui lòng liên hệ AIMS TEAM để được hỗ trợ!");
+        }
+
+        // Show result screen
+        BaseScreenController resultScreen = new ResultScreenController(
+                this.stage,
+                Configs.RESULT_SCREEN_PATH,
+                result.getResult(),
+                result.getMessage()
+        );
         resultScreen.setPreviousScreen(this);
         resultScreen.setHomeScreenHandler(homeScreenHandler);
         resultScreen.setScreenTitle("Result Screen");
         resultScreen.show();
+
 
     }
 
